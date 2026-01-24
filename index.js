99fecbc33937a4b393e13683cb324d7196af9942
Suresh <snt4800nuwa@gmail.com>
2026-01-24 21:32:34 +0530

    remove✏️

--- index.js
+++ index.js
@@ -25,11 +25,7 @@ app.use('/api/stations', StationRouter);
 app.use('/api/stream', proxyRouter);
 
 
-
-/* const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`🚀 Server running on port ${PORT}`);
 });
-*/
-
-export default app;
