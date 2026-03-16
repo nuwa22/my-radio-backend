d47538563da1c0caa82a3cf91c599f61bd9bf258
Suresh <snt4800nuwa@gmail.com>
2026-03-16 14:13:03 +0530

    fixt listning count display error✏️

--- index.js
+++ index.js
@@ -42,6 +42,6 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`🚀 Server running on port ${PORT}`);
 });
